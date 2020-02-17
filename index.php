689993f328682f78d021995237cc10c45827c324
Juliana Ceballos <julianaceballos@nwk-27-19-207.nwk.ruw.rutgers.edu>
2020-02-17 10:34:06 -0500

    Added login form

--- index.php
+++ index.php
@@ -1,8 +1,50 @@
 <?php
+echo'HELLO WORLD';
+?>
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="follow">
+        <title>Page Title</title>
+        
+        <base href="/" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css"/>
+              
+    </head>
+    
+    <body>
+      
+        <div class="uk-section uk-container uk-text-center">
+            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1"uk-grid>
+     <form class="uk-form-stacked js-login">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">Email</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+        </div>
+    </div>
+
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-select">Password</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
+        </div>
+    </div>
+         
+    <div class="uk-margin">
+        <button class="uk-button uk-button-default" type="submit">Login</button>
+    </div>
+      
+</form> 
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>       
+    </body>
+    
+</html>
 
